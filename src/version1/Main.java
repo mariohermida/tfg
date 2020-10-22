1d71fa08b8165565f33bb8110f5f4e92f2194e42
Mario Hermida López <marioher98@gmail.com>
2020-10-22 13:45:23 +0200

    Binary to hexadecimal function and viceversa

--- src/version1/Main.java
+++ src/version1/Main.java
@@ -14,11 +14,48 @@ public class Main {
 
 		Main main = new Main();
 
-		File file = new File("C:\\Users\\Mario Hermida\\git\\tfg\\txt\\words.txt");
+		/*File file = new File("C:\\Users\\Mario Hermida\\git\\tfg\\txt\\words.txt");
 
 		main.readFile(file);
-		main.writeFile(file);
+		main.writeFile(file);*/
+		
+		main.binaryToHexadecimal("11010");
+		main.hexadecimalToBinary("1a");
 
+	}
+	
+	private String binaryToHexadecimal(String binary) {
+		String hexValue = "", substring;
+		//Adapting binary number for being multiple of 4 bits
+		while (binary.length()%4!=0) {
+			binary = "0" + binary;
+		}
+		//Splitting into 4-bit blocks and converting into hexadecimal
+		for (int i = 0; i < binary.length(); i+=4) {
+			substring = "";
+			substring += binary.charAt(i);
+			substring += binary.charAt(i+1);
+			substring += binary.charAt(i+2);
+			substring += binary.charAt(i+3);
+			hexValue += Integer.toHexString(Integer.parseInt(substring, 2));
+		}
+		System.out.println(hexValue);
+		return hexValue;
+	}
+	
+	private String hexadecimalToBinary(String hexadecimalString) {		
+		String binValue = "", hexDigit;
+		for (int i = 0; i < hexadecimalString.length(); i++) {
+			hexDigit = String.valueOf(hexadecimalString.charAt(i));
+			String binary4BitNumber = Integer.toBinaryString((Integer.parseInt(hexDigit, 16)));
+			//Appending zeroes for having multiple of 4 bits number
+			while(binary4BitNumber.length() < 4) {				
+				binary4BitNumber = "0" + binary4BitNumber;
+			}
+			binValue += binary4BitNumber;
+		}
+		System.out.println(binValue);
+		return binValue;
 	}
 
 	private void readFile(File file) {
