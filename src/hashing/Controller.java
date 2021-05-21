070e6657a3ce67424897f132047d54d830d64df3
Mario Hermida López <marioher98@gmail.com>
2021-05-21 09:49:58 +0200

    Application fully working
    All features implemented. Both message hash and file hash work as
    expected

--- src/hashing/Controller.java
+++ src/hashing/Controller.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -37,49 +36,103 @@ public class Controller {
 		if (SHS.getSelectedToggle() == null) {
 			showAlert("No algorithm chosen!");
 		} else {
-			HashFunction sh = null;
-			RadioButton option = (RadioButton) SHS.getSelectedToggle();
-
-			if (option.getText().equals("SHA-1")) {
-				sh = new SHA_1(nameField.getText());
-			} else if (option.getText().equals("SHA-224")) {
-				sh = new SHA_224(nameField.getText());
-			} else if (option.getText().equals("SHA-256")) {
-				sh = new SHA_256(nameField.getText());
-			} else if (option.getText().equals("SHA-384")) {
-				sh = new SHA_384(nameField.getText());
-			} else if (option.getText().equals("SHA-512")) {
-				sh = new SHA_512(nameField.getText());
-			} else if (option.getText().equals("SHA-512/224")) {
-				sh = new SHA_512_224(nameField.getText());
-			} else if (option.getText().equals("SHA-512/256")) {
-				sh = new SHA_512_256(nameField.getText());
-			} else if (option.getText().equals("SHA3-224")) {
-				sh = new SHA_3_224(nameField.getText());
-			} else if (option.getText().equals("SHA3-256")) {
-				sh = new SHA_3_256(nameField.getText());
-			} else if (option.getText().equals("SHA3-384")) {
-				sh = new SHA_3_384(nameField.getText());
-			} else if (option.getText().equals("SHA3-512")) {
-				sh = new SHA_3_512(nameField.getText());
-			} else {
-				if (isOutputLengthAnInteger() && isOutputLengthAValidInteger()) {
-					int outputLength = Integer.parseInt(outputField.getText());
-					// XOF functions
-					if (option.getText().equals("SHAKE128")) {
-						sh = new SHAKE128(nameField.getText(), outputLength);
-					} else {
-						sh = new SHAKE256(nameField.getText(), outputLength);
-					}
-				} else {
-					// Output value is not an integer or the output field is empty
-					sh = null;
-					showAlert("Output length must be an integer number, greater than zero and multiple of 4.");
-				}
-			}
-
+			HashFunction sh = computeHashDependingOnAlgorithmChosen("");
 			if (sh != null) {
 				digestField.setText(sh.computeHash());
+			}
+		}
+	}
+
+	private HashFunction computeHashDependingOnAlgorithmChosen(String fileMessage) {
+		RadioButton option = (RadioButton) SHS.getSelectedToggle();
+
+		if (option.getText().equals("SHA-1")) {
+			if (fileMessage.equals("")) {
+				return new SHA_1(nameField.getText());
+			} else {
+				return new SHA_1(fileMessage, true);
+			}
+		} else if (option.getText().equals("SHA-224")) {
+			if (fileMessage.equals("")) {
+				return new SHA_224(nameField.getText());
+			} else {
+				return new SHA_224(fileMessage, true);
+			}
+		} else if (option.getText().equals("SHA-256")) {
+			if (fileMessage.equals("")) {
+				return new SHA_256(nameField.getText());
+			} else {
+				return new SHA_256(fileMessage, true);
+			}
+		} else if (option.getText().equals("SHA-384")) {
+			if (fileMessage.equals("")) {
+				return new SHA_384(nameField.getText());
+			} else {
+				return new SHA_384(fileMessage, true);
+			}
+		} else if (option.getText().equals("SHA-512")) {
+			if (fileMessage.equals("")) {
+				return new SHA_512(nameField.getText());
+			} else {
+				return new SHA_512(fileMessage, true);
+			}
+		} else if (option.getText().equals("SHA-512/224")) {
+			if (fileMessage.equals("")) {
+				return new SHA_512_224(nameField.getText());
+			} else {
+				return new SHA_512_224(fileMessage, true);
+			}
+		} else if (option.getText().equals("SHA-512/256")) {
+			if (fileMessage.equals("")) {
+				return new SHA_512_256(nameField.getText());
+			} else {
+				return new SHA_512_256(fileMessage, true);
+			}
+		} else if (option.getText().equals("SHA3-224")) {
+			if (fileMessage.equals("")) {
+				return new SHA_3_224(nameField.getText());
+			} else {
+				return new SHA_3_224(fileMessage, true);
+			}
+		} else if (option.getText().equals("SHA3-256")) {
+			if (fileMessage.equals("")) {
+				return new SHA_3_256(nameField.getText());
+			} else {
+				return new SHA_3_256(fileMessage, true);
+			}
+		} else if (option.getText().equals("SHA3-384")) {
+			if (fileMessage.equals("")) {
+				return new SHA_3_384(nameField.getText());
+			} else {
+				return new SHA_3_384(fileMessage, true);
+			}
+		} else if (option.getText().equals("SHA3-512")) {
+			if (fileMessage.equals("")) {
+				return new SHA_3_512(nameField.getText());
+			} else {
+				return new SHA_3_512(fileMessage, true);
+			}
+		} else {
+			if (isOutputLengthAnInteger() && isOutputLengthAValidInteger()) {
+				int outputLength = Integer.parseInt(outputField.getText());
+				// XOF functions
+				if (option.getText().equals("SHAKE128")) {
+					if (fileMessage.equals("")) {
+						return new SHAKE128(nameField.getText(), outputLength);
+					} else {
+						return new SHAKE128(fileMessage, outputLength, true);
+					}
+				} else {
+					if (fileMessage.equals("")) {
+						return new SHAKE256(nameField.getText(), outputLength);
+					} else {
+						return new SHAKE256(fileMessage, outputLength, true);
+					}
+				}
+			} else {
+				// Output value is not an integer or the output field is empty
+				showAlert("Output length must be an integer number, greater than zero and multiple of 4.");
+				return null;
 			}
 		}
 	}
@@ -104,32 +157,38 @@ public class Controller {
 
 	@FXML
 	void fileChooser(ActionEvent event) {
-		// Open fileChooser
-		FileChooser fc = new FileChooser();
-		fc.getExtensionFilters().add(new ExtensionFilter("Files", "*.*"));
-		file = fc.showOpenDialog(null);
+		if (SHS.getSelectedToggle() == null) {
+			showAlert("No algorithm chosen!");
+		} else {
+			// Open fileChooser
+			FileChooser fc = new FileChooser();
+			fc.getExtensionFilters().add(new ExtensionFilter("Files", "*.*"));
+			file = fc.showOpenDialog(null);
 
-		// Obtain the bytes from the file read
-		byte[] bytes;
-		try {
-			bytes = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
-		} catch (IOException e) {
-			bytes = null;
-			e.printStackTrace();
+			// Obtain the bytes from the file read
+			byte[] bytes;
+			try {
+				bytes = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+			} catch (IOException e) {
+				bytes = null;
+				e.printStackTrace();
+			}
+
+			// Convert the bytes to binary
+			HashFunction sh;
+			String binaryCharacter, binaryMessage = "";
+			for (byte b : bytes) {
+				// First 24 bits are ignored, due to 8 bit string is wanted
+				binaryCharacter = Integer.toBinaryString((b & 0xFF) + 0x100).substring(1);
+				binaryMessage += binaryCharacter;
+			}
+
+			// Compute and show the hash
+			sh = computeHashDependingOnAlgorithmChosen(binaryMessage);
+			if (sh != null) {
+				digestField.setText(sh.computeHash());
+			}
 		}
-
-		// Convert the bytes to binary
-		HashFunction sh;
-		String binaryCharacter, binaryMessage = "";
-		for (byte b : bytes) {
-			// First 24 bits are ignored, due to 8 bit string is wanted
-			binaryCharacter = Integer.toBinaryString((b & 0xFF) + 0x100).substring(1);
-			binaryMessage += binaryCharacter;
-		}
-
-		// Compute and show the hash
-		sh = new SHA_3_256(binaryMessage, true);
-		digestField.setText(sh.computeHash());
 	}
 
 	private void showAlert(String message) {
--- src/hashing/Main.java
+++ src/hashing/Main.java
@@ -29,6 +29,7 @@ public class Main extends Application {
 		}
 		Scene scene = new Scene(mainLayout);
 		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
 		primaryStage.show();
 	}
 
